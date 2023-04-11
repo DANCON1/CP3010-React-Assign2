8ba77262ca1d32ddcfa9cac791e0fc6ef5732f43
DANCON1 <111096403+DANCON1@users.noreply.github.com>
2023-04-11 10:15:33 -0230

    Update server.js
    Moved hard-coded json to file, added functionality to add and save new movie to file

--- new-movies-backend/src/server.js
+++ new-movies-backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
+const port = 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
@@ -8,69 +10,8 @@ app.get('/hello', (req, res) => {
     res.send('Hello!');
 });
 
-app.post('/hello', (req, res) => {
-    console.log(req.body);
-    res.send('What');
-});
-
-let movieData = [
-    {
-        "id": 1,
-        "name": "Nosferatu the Vampyre",
-        "releaseDate": "1979-01-01",
-        "actors": "Klaus Kinski, Isabelle Adjani, Bruno Ganz",
-        "posterImage": "https://upload.wikimedia.org/wikipedia/en/9/95/Nosferatu_Phantom_der_Nacht.jpg",
-        "rating": 5
-      },
-      {
-        "id": 2,
-        "name": "Aguirre: The Wrath of God",
-        "releaseDate": "1972-12-29",
-        "actors": "Klaus Kinski, Helena Rojo, Del Negro",
-        "posterImage": "https://upload.wikimedia.org/wikipedia/en/a/a7/AguirreGermanPoster_.jpg",
-        "rating": 4.8
-      },
-      {
-        "id": 3,
-        "name": "Fitzcarraldo",
-        "releaseDate": "1982-06-30",
-        "actors": "Klaus Kinski, Claudia Cardinale, José Lewgoy",
-        "posterImage": "https://upload.wikimedia.org/wikipedia/en/a/ae/Fitzcarraldo.jpg",
-        "rating": 5
-      },
-      {
-        "id": 4,
-        "name": "The Shining",
-        "releaseDate": "1980-05-23",
-        "actors": "Jack Nicholson, Shelley Duvall, Danny Lloyd",
-        "posterImage": "https://upload.wikimedia.org/wikipedia/en/1/1d/The_Shining_%281980%29_U.K._release_poster_-_The_tide_of_terror_that_swept_America_IS_HERE.jpg",
-        "rating": 5
-      },
-      {
-        "id": 5,
-        "name": "Manhunter",
-        "releaseDate": "1986-08-15",
-        "actors": "William Petersen, Kim Greist, Joan Allen",
-        "posterImage": "https://upload.wikimedia.org/wikipedia/en/7/77/Manhunter_michael_mann_film_poster.jpg",
-        "rating": 4.7
-      },
-      {
-        "id": 6,
-        "name": "Bad Lieutenant Port of Call: New Orleans",
-        "releaseDate": "2009-09-09",
-        "actors": "Nicolas Cage, Eva Mendes, Val Kilmer",
-        "posterImage": "https://upload.wikimedia.org/wikipedia/en/9/9f/Bad_lieutenant.jpeg",
-        "rating": 4.5
-      },
-      {
-        "id": 7,
-        "name": "Jason and the Argonauts",
-        "releaseDate": "1963-06-19",
-        "actors": "Todd Armstrong, Nancy Kovack, Gary Raymond",
-        "posterImage": "https://upload.wikimedia.org/wikipedia/commons/6/62/Jason_and_the_argounauts.jpg",
-        "rating": 3.8
-      }
-]
+const movieData = JSON.parse(fs.readFileSync('./movies.json'));
+//console.log(movieData);
 
 app.get('/movies', (req, res) => {
     res.json(movieData);
@@ -85,9 +26,20 @@ app.post('/submission', (req,res) => {
         "posterImage":req.body.poster,
         "rating":req.body.rating
     })
+    saveData();
     console.log(req.body);
     res.redirect('/');
-})
+});
+
+const saveData = () => {
+    const jsonContent = JSON.stringify(movieData);
+    fs.writeFile("./movies.json", jsonContent, 'utf8', function (err) {
+        if (err){
+            console.log("An error occured trying to write JSON object to file.");
+        }
+        console.log("JSON file saved");
+    });
+}
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');

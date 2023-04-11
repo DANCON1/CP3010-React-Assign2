497c12eab2fd73b7cbb5a26df691e63bbcd51652
DANCON1 <111096403+DANCON1@users.noreply.github.com>
2023-04-11 12:34:50 -0230

    Update server.js
    Added mongoDB functionality

--- new-movies-backend/src/server.js
+++ new-movies-backend/src/server.js
@@ -1,46 +1,51 @@
 import express from 'express';
-import fs from 'fs';
+import {MongoClient } from 'mongodb';
 
 const app = express();
 const port = 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
+app.get('/movies', async (req, res) => {
+
+    console.log('get!');
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('movies');
+
+    const movieData = await db.collection('movie').find({}).toArray();
+
+    res.json(movieData);
+})
+
 app.get('/hello', (req, res) => {
     res.send('Hello!');
 });
 
-const movieData = JSON.parse(fs.readFileSync('./movies.json'));
-//console.log(movieData);
+app.post('/submission', async (req,res) => {
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
 
-app.get('/movies', (req, res) => {
-    res.json(movieData);
-});
+    const db = client.db('movies');
 
-app.post('/submission', (req,res) => {
-    movieData.push( {
-        "id":movieData.length+1,
+     // Find the last inserted document and get the highest "id" value
+     const lastDocument = await db.collection('movie').findOne({}, {sort: {id: -1}});
+     const highestId = lastDocument ? lastDocument.id + 1 : 1;
+
+    const insertOperation = await db.collection('movie').insertOne({
+        "id":highestId,
         "name":req.body.title,
         "releaseDate":req.body.releaseDate,
         "actors":req.body.actors,
         "posterImage":req.body.poster,
         "rating":req.body.rating
     })
-    saveData();
-    console.log(req.body);
+    
     res.redirect('/');
 });
 
-const saveData = () => {
-    const jsonContent = JSON.stringify(movieData);
-    fs.writeFile("./movies.json", jsonContent, 'utf8', function (err) {
-        if (err){
-            console.log("An error occured trying to write JSON object to file.");
-        }
-        console.log("JSON file saved");
-    });
-}
-
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });

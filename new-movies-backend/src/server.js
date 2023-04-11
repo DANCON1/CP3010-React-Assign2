c8dd7fe0ef9bd5a836eeabedfdd99771cf134ff6
DANCON1 <111096403+DANCON1@users.noreply.github.com>
2023-04-11 13:37:14 -0230

    Added mongo delete and file upload functionality

--- new-movies-backend/src/server.js
+++ new-movies-backend/src/server.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import {MongoClient } from 'mongodb';
+import multer from 'multer';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 const app = express();
 const port = 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+
+app.use(express.static(path.join(__dirname, '../posters')));
+
+const upload = multer({ dest: 'posters/' })
 
 app.get('/movies', async (req, res) => {
 
@@ -24,7 +34,7 @@ app.get('/hello', (req, res) => {
     res.send('Hello!');
 });
 
-app.post('/submission', async (req,res) => {
+app.post('/submission', upload.single('movie_poster'), async (req,res) => {
     const client = new MongoClient('mongodb://127.0.0.1:27017');
     await client.connect();
 
@@ -39,12 +49,21 @@ app.post('/submission', async (req,res) => {
         "name":req.body.title,
         "releaseDate":req.body.releaseDate,
         "actors":req.body.actors,
-        "posterImage":req.body.poster,
+        "posterImage":req.file.filename,
         "rating":req.body.rating
     })
     
     res.redirect('/');
 });
+
+app.post('/api/delete', async (req,res)=>{
+    const data = req.body.data.split(',');
+    const client = new MongoClient('mongodb://127.0.0.1:27017/');
+    await client.connect();
+    const db = client.db('movies');
+    const deleteOperation = await db.collection('movie').deleteOne({'name':data[0],'releaseDate':data[1],'actors':data[2]})
+    res.redirect('/');
+})
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
